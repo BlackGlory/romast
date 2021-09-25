857c9b0a2797fe1799243b4851813d7f37d1b00e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-09-25 18:35:05 +0000

    fix: https://github.com/orgapp/orgajs/issues/147

--- src/parse.ts
+++ src/parse.ts
@@ -4,7 +4,14 @@ import { parse as orga } from 'orga'
 import { transform } from '@src/transform-oast-to-romast'
 
 export function parse(text: string): ROMAST.Document {
-  const oast = orga(text) as OAST.Document
+  const oast = orga(dosToUnix(text)) as OAST.Document
   const romast = transform(oast)
   return romast
+}
+
+/**
+ * @see https://github.com/orgapp/orgajs/issues/147
+ */
+function dosToUnix(text: string): string {
+  return text.replace(/\r\n/g, '\n')
 }
